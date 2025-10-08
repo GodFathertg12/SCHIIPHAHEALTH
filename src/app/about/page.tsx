546fff647edb7bb56f02c5ac304f58d90dfaf6fb
Isaac Olaye <isaacolaye0@gmail.com>
2025-10-08 10:03:24 -0700

    fix: wrong spellings

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,12 @@ export default function AboutPage() {
       <div className="max-w-3xl w-full space-y-6">
         <h1 className="text-4xl font-bold text-center text-green-800">About Schiipha</h1>
         <p className="text-lg leading-relaxed">
-            At Schiipha, we're redefining hygiene and comfort in public spaces. Founded by a team a team of young innovators passionate about
+            At Schiipha, we're redefining hygiene and comfort in public spaces. Founded by a team of young innovators passionate about
             public health, our mission is simple: to make hygiene accessible, practical, and sustainable for everyone.
         </p>
         <p className="text-lg leading-relaxed">
           Our flagship product, Bum's Hero, is designed to protect users from harmful bacteria found on toilet seats. Made from soft, antimicrobial,
-          and biodegradable materials, it provides a clean, safe, safe layer of protection that's easy to use and perfect for restrooms in schools, offices, hospitals, and 
+          and biodegradable materials, it provides a clean, safe layer of protection that's easy to use and perfect for restrooms in schools, offices, hospitals, and 
           other high-traffic facilities.
         </p>
         <p className="text-lg leading-relaxed">
